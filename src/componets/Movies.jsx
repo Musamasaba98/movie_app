b01b7c84f89220a8667a19f10bd5a48809a1a9b6
Masaba Musa <masamata98@gmail.com>
2022-12-15 22:09:55 +0300

    show movies from imdb api

---
+++ src/componets/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Movie from './Movie'
+
+function Movies({title,overview,poster,vote_count,release_date}) {
+  return (
+    <div>
+        <Movie />
+    </div>
+  )
+}
+
+export default Movies
