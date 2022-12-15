00af4e446e8e4619e66fa1d3ea70b2a8d079a967
Masaba Musa <masamata98@gmail.com>
2022-12-15 22:26:00 +0300

    consume api and show movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Movie from './componets/Movie';
+
 
 function App() {
+  const API_URL="https://api.themoviedb.org/3/trending/all/week?api_key=39c5c02d12f04b7ad749d677aa191591"
+  const base_url="http://image.tmdb.org/t/p/w185"
+  let [movies,setMovies]=useState([])
+  useEffect(()=>{
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results);
+    });
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MOVIES LIST</h1>
+      {
+        movies.map((movie,key)=>
+        <Movie key={key} title={movie.title||movie.name} 
+        overview={movie.overview} poster={base_url + movie.poster_path} 
+        vote_count={movie.vote_count} release_date={movie.release_date}/>
+        )
+      }
+
+      
     </div>
   );
 }
