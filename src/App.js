fc8d91854afcbf2d14f075cd0517e0eb36837a33
Masaba Musa <masamata98@gmail.com>
2022-12-16 02:35:20 +0300

    Complete movie-card Design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const API_URL="https://api.themoviedb.org/3/trending/all/week?api_key=39c5c02d12f04b7ad749d677aa191591"
   const base_url="http://image.tmdb.org/t/p/w185"
   let [movies,setMovies]=useState([])
+  
   useEffect(()=>{
     fetch(API_URL)
     .then((res) => res.json())
@@ -16,16 +17,16 @@ function App() {
   },[])
   return (
     <div className="App">
-      <h1>MOVIES LIST</h1>
-      {
+      <h1>TRENDING MOVIES</h1>
+      <div className='movies'>
+        {
         movies.map((movie,key)=>
         <Movie key={key} title={movie.title||movie.name} 
         overview={movie.overview} poster={base_url + movie.poster_path} 
         vote_count={movie.vote_count} release_date={movie.release_date}/>
         )
       }
-
-      
+      </div>  
     </div>
   );
 }
--- src/componets/Movie.jsx
+++ src/componets/Movie.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import '../movie.css'
+import React, { useState } from "react";
+import "../movie.css";
+import { FaHeart} from "react-icons/fa";
 
-function Movie({title,overview,poster,vote_count,release_date}) {
-    
+function Movie({ title, overview, poster, vote_count, release_date }) {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className="movie-item">
-      <img className='poster_img' src={poster} alt="poster_img" /> 
-      <div className="movie-item__overlay">
-        <div className="movie-item__description">
-        <h3 className='movie-item__description--title'>{title}</h3>
-        <div className="movie-item__description--summary">
-          <p>{overview}</p>  
+      <img
+        className="poster_img"
+        src={poster}
+        alt="poster_img"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      />
+        <div className="movie-item__description"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+        >
+          <h3 className="movie-item__description--title">
+            {title.split(" ").length > 5
+              ? title.split(" ").splice(0, 5).join(" ")
+              : title.split(" ").join(" ")}
+          </h3>
+          {
+            isShown &&<>
+            <div className="movie-item__description--summary">
+            <p>{overview.split(" ").splice(0, 15).join(" ") + "..."}</p>
+          </div>
+          <div className="movie-item__description--statistics">
+            <span><FaHeart className="FaHeart"/>{vote_count}</span>
+            <span className="date">{release_date}</span>
+          </div>
+            </>
+            
+          }
+          
         </div>
-        <div className="movie-item__description--statistics">
-            <span>{vote_count}</span>
-            <span>{release_date}</span>
-        </div>
-      </div> 
-      </div>
       
     </div>
-  )
+  );
 }
 
-export default Movie
+export default Movie;
